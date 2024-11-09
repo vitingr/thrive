2ab945e90c33a93222e13d9a3f098e490fffe0c1
vitorgsilvax <vgsilva@tractian.com>
2024-11-09 00:45:18 -0300

    feat(next-intl): update existing content to adapt with locales

--- src/app/(pages)/(feed)/components/Feed/index.tsx
+++ src/app/(pages)/(feed)/components/Feed/index.tsx
@@ -8,7 +8,7 @@ export const Feed: React.FC<FeedProps> = () => {
   return (
     <Container
       as="section"
-      className="flex justify-between gap-6 px-4 py-12 lg:gap-12 lg:py-20 xl:px-0"
+      className="flex justify-between gap-6 py-12 lg:gap-12 lg:py-20 xl:px-0"
       data-cid="homepage-feed"
     >
       <PostFeed />
